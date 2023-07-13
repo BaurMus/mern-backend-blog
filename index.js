b97b6113c6e025f7bb1b33c5dcfb125019dd261c
BaurMus <musilimov_b@mail.ru>
2023-07-13 12:52:25 +0600

    create User model and UserController logic and create function register for registration users

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import {UserController} from './controllers/index.js';
+
+mongoose
+  .connect('mongodb+srv://musilimovb:bake1984@cluster0.rowds20.mongodb.net/blog?retryWrites=true&w=majority')
+  .then(() => console.log('DataBase OK'))
+  .catch((err) => console.log('DataBase Error', err));
+
+const app = express();
+
+app.use(express.json());
+
+app.post('/auth/register', UserController.register);
+
+app.listen(4444, (err) => {
+  if (err) {
+    return console.log(err);
+  }
+
+  console.log("Server OK");
+});
