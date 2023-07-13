172dd594dd25d26ff2ef3532231d3b6e214b4c97
BaurMus <musilimov_b@mail.ru>
2023-07-13 13:38:17 +0600

    create validations with egister validation and handleValidationErrors added them to UserRegistration

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import {UserController} from './controllers/index.js';
+import { registerValidation } from './validations.js';
+import {handleValidationErrors} from './utils/index.js';
 
 mongoose
   .connect('mongodb+srv://musilimovb:bake1984@cluster0.rowds20.mongodb.net/blog?retryWrites=true&w=majority')
@@ -11,7 +13,7 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/auth/register', UserController.register);
+app.post('/auth/register', registerValidation, handleValidationErrors, UserController.register);
 
 app.listen(4444, (err) => {
   if (err) {
