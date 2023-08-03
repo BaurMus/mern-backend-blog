df46c913100db65b3fe7d541bf7e0b3c7e4d2a7e
BaurMus <musilimov_b@mail.ru>
2023-08-03 14:32:46 +0600

    routes separate from server

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import multer from 'multer';
-import cors from 'cors';
 
-import {PostController, UserController} from './controllers/index.js';
-import { loginValidation, postCreateValidation, registerValidation } from './validations.js';
-import {handleValidationErrors, checkAuth} from './utils/index.js';
+import cors from 'cors';
+import routes from './routes.js';
 
 mongoose
   .connect('mongodb+srv://musilimovb:bake1984@cluster0.rowds20.mongodb.net/blog?retryWrites=true&w=majority')
@@ -14,38 +11,9 @@ mongoose
   .catch((err) => console.log('DataBase Error', err));
 
 const app = express();
+
 app.use(cors());
-const storage = multer.diskStorage({
-  destination: (req, file ,cb) => {
-    cb(null, "uploads");
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.originalname);
-  }
-});
-
-const upload = multer({storage});
-
-app.use(express.json());
-app.use('/uploads', express.static('uploads'));
-
-app.post('/auth/register', registerValidation, handleValidationErrors, UserController.register);
-app.post('/auth/login',loginValidation, handleValidationErrors, UserController.login);
-app.get('/auth/me', checkAuth, UserController.getMe);
-
-app.get('/posts', PostController.getAll);
-app.get('/posts/popular', PostController.getAllPopular);
-app.get('/tags', PostController.getLastTags);
-app.get('/posts/:id', PostController.getOne);
-app.post('/posts', checkAuth, postCreateValidation, handleValidationErrors, PostController.create);
-app.delete('/posts/:id', checkAuth, PostController.remove);
-app.patch('/posts/:id', checkAuth, postCreateValidation, handleValidationErrors, PostController.update);
-
-app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
-  res.json({
-    url: `/uploads/${req.file.originalname}`
-  });
-});
+app.use(routes);
 
 app.listen(4444, (err) => {
   if (err) {
---
+++ routes.js
@@ -0,0 +1,46 @@
+import express from 'express';
+import multer from 'multer';
+
+import {PostController, UserController} from './controllers/index.js';
+import { loginValidation, postCreateValidation, registerValidation } from './validations.js';
+import {handleValidationErrors, checkAuth} from './utils/index.js';
+
+const router = express.Router();
+
+const storage = multer.diskStorage({
+  destination: (req, file ,cb) => {
+    cb(null, "uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  }
+});
+
+const upload = multer({storage});
+
+router.use(express.json());
+router.use('/uploads', express.static('uploads'));
+
+//Auth routes
+router.post('/auth/register', registerValidation, handleValidationErrors, UserController.register);
+router.post('/auth/login',loginValidation, handleValidationErrors, UserController.login);
+router.get('/auth/me', checkAuth, UserController.getMe);
+
+//Post routes
+router.get('/posts', PostController.getAll);
+router.get('/posts/popular', PostController.getAllPopular);
+router.get('/tags', PostController.getLastTags);
+router.get('/posts/:id', PostController.getOne);
+router.post('/posts', checkAuth, postCreateValidation, handleValidationErrors, PostController.create);
+router.delete('/posts/:id', checkAuth, PostController.remove);
+router.patch('/posts/:id', checkAuth, postCreateValidation, handleValidationErrors, PostController.update);
+
+//Upload route
+router.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+  res.json({
+    url: `/uploads/${req.file.originalname}`
+  });
+});
+
+export default router; 
+
